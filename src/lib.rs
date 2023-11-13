9b6f944868ccc99632f3f762ddcf69008e0eb3dc
Bohdan Tsehelnyk <mail.qewby@gmail.com>
2023-11-14 01:56:51 +0200

    write parse function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,63 @@
+use pest::error::Error;
+use pest::iterators::{Pair, Pairs};
+use pest::Parser;
 use pest_derive::Parser;
 
 #[derive(Parser)]
-#[grammar = "./constituency.pest"]
+#[grammar = "constituency.pest"]
 pub struct ConstituencyTreeParser;
+
+#[derive(Debug, PartialEq)]
+pub struct ConstituencyTree {
+    value: Option<String>,
+    tag: Option<String>,
+    items: Option<Vec<ConstituencyTree>>,
+}
+
+impl ConstituencyTree {
+    pub fn new() -> Self {
+        ConstituencyTree {
+            value: None,
+            tag: None,
+            items: None,
+        }
+    }
+
+    fn parse_tree_node(pair: Pair<Rule>) -> ConstituencyTree {
+        let mut node = ConstituencyTree::new();
+
+        match pair.as_rule() {
+            Rule::leaf_node => {
+                let mut inner_pairs: Pairs<'_, Rule> = pair.into_inner();
+                node.tag = Some(inner_pairs.next().unwrap().as_str().to_string());
+                node.value = Some(inner_pairs.next().unwrap().as_str().to_string());
+            }
+            Rule::node => {
+                let mut inner_pairs = pair.into_inner();
+                node.tag = Some(inner_pairs.next().unwrap().as_str().to_string());
+                node.items = Some(inner_pairs.map(Self::parse_tree_node).collect());
+            }
+            _ => {
+                unreachable!()
+            }
+        }
+
+        node
+    }
+
+    pub fn parse(input: String) -> Result<ConstituencyTree, Box<Error<Rule>>> {
+        let tree = ConstituencyTreeParser::parse(Rule::tree, &input)?
+            .next()
+            .unwrap()
+            .into_inner()
+            .next()
+            .unwrap();
+        Ok(Self::parse_tree_node(tree))
+    }
+}
+
+impl Default for ConstituencyTree {
+    fn default() -> Self {
+        ConstituencyTree::new()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,6 @@
-fn main() {}
+use constituency_tree_parser::ConstituencyTree;
+fn main() {
+    let input = String::from("(S ");
+    let tree = ConstituencyTree::parse(input);
+    println!("{:#?}", tree);
+}
