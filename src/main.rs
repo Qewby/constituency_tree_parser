a28e17bf3a41448c1acf887c6af4106bdb3b2758
Bohdan Tsehelnyk <mail.qewby@gmail.com>
2023-11-20 04:49:57 +0200

    add cli

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
+use clap::{Arg, Command};
 use constituency_tree_parser::ConstituencyTree;
+
 fn main() {
-    let input = String::from("(S ");
-    let tree = ConstituencyTree::parse(input);
-    println!("{:#?}", tree);
+    //let tree = ConstituencyTree::parse(input);
+    //println!("{:#?}", tree);
+    let current_version = env!("CARGO_PKG_VERSION");
+
+    let matches = Command::new("Constituency Tree Parser")
+        .version(current_version)
+        .author("Bohdan Tsehelnyk")
+        .about("Parse text constituency tree into data structure")
+        .arg(Arg::new("parse").short('p').help("Parse tree"))
+        .get_matches();
+
+    let prase_str = matches.get_one::<String>("parse");
+    match prase_str {
+        None => println!("No subcommand provided. Use -h for help."),
+        Some(input) => {
+            match ConstituencyTree::parse(String::from(input)) {
+                Ok(tree) => {
+                    println!("Parsed Constituency Tree:\n{:#?}", tree);
+                    // You can now work with the parsed tree as needed
+                }
+                Err(err) => {
+                    eprintln!("Error parsing constituency tree: {}", err);
+                }
+            }
+        }
+    }
 }
